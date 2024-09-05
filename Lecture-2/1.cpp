16634fe1c47c79852e4f7265ebb3bd5af4e89acb
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-09-05 12:45:55 +0600

    complete lecture-2

---
+++ Lecture-2/1.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    // int age=25; // 4 bytes
+    // char grade = 'A'; //  1 bytes
+    // float PI = 3.4f; // 4 bytes
+    // bool isSafe=true; // 1 byte
+
+    // cout<<age<<endl;
+    // cout<<PI<<endl;
+    // cout<<isSafe<<endl;
+
+    // int age;
+    // cout<<"Enter your age : ";
+    // cin>>age;
+    // cout<<"Your age is : "<<age<<endl;
+
+    // int a=10 ,b =5;
+    // cout << "sum" << (a+b)<<endl;
+
+    // cout << "difference" << (a-b)<<endl;
+
+    // cout << "product" << (a*b)<<endl;
+
+    // cout << "divison" << (a/b)<<endl;
+
+    // cout << "remainder" << (a%b)<<endl;
+
+    // int a,b;
+    // cout<<"Enter two numbers : ";
+
+    // cin>>a>>b;
+    // int sum =a+b;
+    
+    // cout<<"sum : "<<sum<<endl;
+
+    return 0;
+}
