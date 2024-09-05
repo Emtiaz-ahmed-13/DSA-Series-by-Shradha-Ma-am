f60d52d7962bbf9e42b5ee49992dc49c4e6e1352
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-09-05 13:01:19 +0600

    complete lecture-03

---
+++ Lecture-3/practice-probelm.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // int n=50;
+    // int sum=0;
+    // for(int i=1;i<=n;i++){
+    //     sum+=1;
+    //     if(i==5)
+    //         break;
+    // }
+
+    // cout<<"sum " <<sum <<endl;
+
+
+
+    // int n=10;
+    // for(int i=1;i<=n;i++){
+    //     if(i%2 !=0){
+    //         cout<<i<<" ";
+    //     }
+    // }
+    // cout<<endl;
+
+    // int n=7;
+    // bool isPrime=true;
+    // for(int i=2;i<=n;i++){
+    //     if(n%i==0){
+    //         isPrime=false;
+    //         break;
+    //     }
+    // }
+    // if(isPrime){
+    //     cout<<"Prime number"<<endl;
+    // }
+    // else{
+    //     cout<<"Not Prime number"<<endl;
+    // }
+    // int n;
+    // cin>>n;
+
+    // for (int i = 1; i <= n; i++) {
+    //     int m = 10; 
+
+    //     for (int j = 1; j <= m; j++) { 
+    //         cout << "*";
+    //     }
+
+    //     cout << endl;
+    // }
+
+
+    // return 0;
+
+}
