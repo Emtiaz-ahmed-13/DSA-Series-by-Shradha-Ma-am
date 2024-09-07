b1f947e6975cf42ee228f02caf499b9221be393a
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-09-08 00:52:59 +0600

    finish lecture-4

--- Lecture-4/10.cpp
+++ Lecture-4/10.cpp
@@ -1,21 +1,19 @@
 /**************************************************/
 /*           Author : Nightshade Emtiaz           */
-/*        Github : github.com/Emtiaz-ahmed-13      */
+/*        Github : github.com/Emtiaz-ahmed-13   */
 /**************************************************/
-
-
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int n =5; 
-
+int main()
+{
+    int n=4;
     for(int i=0;i<n;i++){
-        for(int j=i+1;j>=0;j--){
-            cout << j << " ";
+        int num=1; // Move num initialization here
+        for(int j=i+1;j>0;j--){
+            cout<<num<<" ";
+            num++;
         }
-        cout << endl;
+        cout<<endl;
     }
-
     return 0;
 }
