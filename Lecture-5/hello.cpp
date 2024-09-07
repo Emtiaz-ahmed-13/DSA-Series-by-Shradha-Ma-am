47d42af4d07a4b74233c0cbb3e5cbd36e3f64842
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-09-08 01:34:00 +0600

    finish-lecture-5

---
+++ Lecture-5/hello.cpp
@@ -0,0 +1,17 @@
+/**************************************************/
+/*           Author : Nightshade Emtiaz           */
+/*        Github : github.com/Emtiaz-ahmed-13   */
+/**************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+int printHello(){
+    cout<<"Hello World!"<<endl;
+    return 3;
+}
+int main()
+{
+
+    int value = printHello();
+    cout << value << endl;
+    return 0;
+}
