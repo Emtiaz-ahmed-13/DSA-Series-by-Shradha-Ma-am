f1059e47b6672d188194b86cb64ad4a17ccc82c2
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-10-19 17:37:14 +0600

    string part-1

---
+++ String/part-1/1.cpp
@@ -0,0 +1,14 @@
+/**************************************************/
+/*           Author : Nightshade Emtiaz           */
+/*        Github : github.com/Emtiaz-ahmed-13   */
+/**************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char str[] ={'a','b','c'};
+    int arr[]={1,2,3,4,5,6,7,8,9,10};
+    cout << strlen(str) << endl;
+    
+    return 0;
+}
