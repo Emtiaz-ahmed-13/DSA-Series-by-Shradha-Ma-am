7344da134fef8c64ad88b7607baf828182bc265f
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2024-10-20 19:32:30 +0600

    done

--- String/part-1/1.cpp
+++ String/part-1/1.cpp
@@ -9,6 +9,14 @@ int main()
     char str[] ={'a','b','c'};
     int arr[]={1,2,3,4,5,6,7,8,9,10};
     cout << strlen(str) << endl;
+
+    char str[100];
+    cout<< : "Enter a string : ";
+    cin.getline(str,100);
+    cin>>str;
+
+
+    cout<<"output : "<<str<<endl;
     
     return 0;
 }
---
+++ String/part-1/2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str = "apna college"; //dynamic = runtime resize
+    cout << str << endl;
+
+
+    str="hello";
+    
+    return 0;
+}
